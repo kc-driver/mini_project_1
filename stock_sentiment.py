18f9b874c53c797bf2caf9e2501fa12f203a1504
kc <khoonching@gmail.com>
2021-07-20 00:05:16 +0800

    'day1-scrape'

--- stock_sentiment.py
+++ stock_sentiment.py
@@ -1,7 +1,30 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import datetime
 
-#scrape topglove comments
-url = 'https://klse.i3investor.com/servlets/forum/800001345.jsp?ftp=400'
-html = get(url)
-sp = BeautifulSoup(html.text, 'html.parser')
+#open page
+driver = webdriver.Chrome()
+url='https://klse.i3investor.com/servlets/forum/800001345.jsp?ftp=400'
+driver.get(url)
+
+#bs4 container
+soup = BeautifulSoup(driver.page_source,'html.parser')
+list_sp = soup.find_all('td',attrs={'style':'word-wrap: break-word;'})
+
+
+user=[]
+date=[]
+comt=[]
+
+for li in list_sp:
+    name_sp = li.find('span',attrs={'class':'comuid'})
+    print(name_sp.text)
+    user.append(name_sp)
+
+    date_sp = li.find('span',attrs={'class':'comdt'})
+    print(date_sp.text)
+    date.append(date_sp)
+
+    comment_sp = li.find('span',attrs={'class':'autolink'})
+    print(comment_sp.text)
+    comt.append(comment_sp)
